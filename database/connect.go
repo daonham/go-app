ffdd5c90abbc389b4448ca0e18f330c823bc6411
daonham <daonham95@gmail.com>
2021-10-07 23:53:00 +0700

    first commit

---
+++ database/connect.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"database/sql"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ConnectDB(db *sql.DB) {
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return db
+}
